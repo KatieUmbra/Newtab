d401a681e3e6d1ccede06d8c2c9cad879ea69924
Katherine <katherine@kaytea.dev>
2024-11-29 11:27:10 -0500

    wip: basic layout

---
+++ src/form.ts
@@ -0,0 +1,26 @@
+const form = document.getElementById("search-form") as HTMLFormElement;
+const formTxtbox = document.getElementById("search-box") as HTMLInputElement;
+
+addEventListener("focus", (_) => {
+    formTxtbox.focus();
+});
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const data = new FormData(form);
+    const inputText = data.get("query") as string;
+    let url = "";
+    if (inputText == "") {
+        formTxtbox.focus();
+        return;
+    };
+    if (URL.canParse(inputText)) url = inputText;
+    else {
+        console.log("URL is invalid!");
+        const searchEncoded = encodeURI(inputText);
+        url = `https://www.startpage.com/sp/search?query=${searchEncoded}`;
+    }
+    window.open(url, "_blank")?.focus();
+    formTxtbox.value = "";
+}
---
+++ src/link.ts
@@ -0,0 +1,22 @@
+const links = new Map<number, string>([
+    [1, "https://mail.google.com/"],
+    [2, "https://outlook.live.com/mail/"],
+    [3, "https://mail.proton.me/"],
+    [4, "https://www.amazon.com/"],
+    [5, "https://www.youtube.com/"],
+    [6, "https://bsky.app/"],
+    [7, "https://www.pinterest.com/"],
+    [8, "https://github.com/"],
+    [9, "https://drive.google.com/"],
+    [10, "https://onedrive.live.com/"],
+    [11, "https://roblox.com/"],
+    [12, "https://chess.com"]
+]);
+
+links.forEach((value: string, key: number) => {
+    const buttonId = `link-${key}`;
+    const button = document.getElementById(buttonId) as HTMLImageElement;
+    button.addEventListener("click", (_) => {
+        window.open(value, "_blank")?.focus();
+    })
+})
